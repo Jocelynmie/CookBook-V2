53fd3e9c5d0be92f615401c094dd0fea462def78
Jocelyn Yang <127737447+Jocelynmie@users.noreply.github.com>
2025-03-17 18:45:05 -0700

    Delete cookbook directory

--- cookbook/api/index.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-
-console.log("API入口点加载成功");
-console.log("当前工作目录:", process.cwd());
-
-export default app;
--- cookbook/frontend/src/components/WeeklyMealPlan.jsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-function WeeklyMealPlan() {
-  return <div></div>;
-}
-
-export default WeeklyMealPlan;
