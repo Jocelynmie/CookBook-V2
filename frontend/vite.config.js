978c0cc38d4685ea4f2be7d7e0b709cb07469184
jocleyn123 <jocelynyang414@outlook.com>
2025-03-18 10:51:29 -0700

    ready for deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,41 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:8080",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+//   build: {
+//     outDir: "dist",
+//     emptyOutDir: true,
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:8080",
         changeOrigin: true,
-        secure: false,
       },
     },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
   },
 });
